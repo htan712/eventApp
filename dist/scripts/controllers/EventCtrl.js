b8107666131e79c03b2253eefa1592c1606240e1
htan712 <hock.tan712@gmail.com>
2016-08-19 13:40:42 -0700

    learned checkbox and ng-if

--- dist/scripts/controllers/EventCtrl.js
+++ dist/scripts/controllers/EventCtrl.js
@@ -1,12 +1,32 @@
 (function() {
     function EventCtrl(eventFactory) {
-
+        
+        var self = this;
         this.event = eventFactory.getAllEvents();
+        
         this.categories = [{id:1, name:'Music', group:'Main'}, {id:2, name:'Cinema', group:'Main'}, {id:3, name:'Games', group:'Others'}, {id:4, name:'Special Category', group:'Others'}];
+        
+        this.selectedOption = {id:1, name:'Music', group:'Main'};
+        this.specialEvent = 'false';
+        this.specialValue = {id:1, value:'somethingSpecial'};
+        
+        this.specialType = [{name: 'Age Restricted', checked: false}, {name: 'Luxury', checked:false}];
+        
+        this.selectAllTypes = function() {
+            if (self.bothSelected) {
+                self.bothSelected = true;
+            } else {
+                self.bothSelected = false;
+            }
+            
+            angular.forEach(self.specialType, function(item) {
+                item.checked = self.bothSelected;
+            })
+        }
 
         this.submitForm = function (form){
+            form.category = this.selectedOption.id;
             eventFactory.createEvent(angular.copy(form), this.event);
-            console.log(this.event);
         }
     }
 
